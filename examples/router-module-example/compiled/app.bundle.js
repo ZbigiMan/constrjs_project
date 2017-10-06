0ef0618760257cf0227eb04a86c45efd61a926d8
Zbigi Man Zbigniew Stępniewski <zbigiman@gmail.com>
2017-10-06 13:17:02 +0200

    examples update

--- examples/router-module-example/compiled/app.bundle.js
+++ examples/router-module-example/compiled/app.bundle.js
@@ -73,34 +73,30 @@
 "use strict";
 
 
-!function () {
-  "use strict";
-  var t = function t(_t, o) {
-    o = o || window;for (var i = 0; i < this.length; i++) {
-      _t.call(o, this[i], i, this);
-    }
-  };window.NodeList && !NodeList.prototype.forEach && (NodeList.prototype.forEach = t);
-}();
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.RouterModule = undefined;
 
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__source_constrjs_dom_module_dom_module__ = __webpack_require__(8);
-// RouterModule
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); // RouterModule
 // ES6+ client router | :{constrjs} project
 // Author: Zbigi Man Zbigniew Stępniewski 2017
-//import { DOMMModule } from '@zbigiman/constrjs.dom.module';
 
 
-class RouterModule {
-    constructor(settings) {
+var _constrjsDom = __webpack_require__(4);
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+//import {DOMModule} from '../../source/constrjs.dom.module/dom.module';
+
+var RouterModule = exports.RouterModule = function () {
+    function RouterModule(settings) {
+        _classCallCheck(this, RouterModule);
+
         this.base = settings.base || '/';
         if (settings.SPAEmulation === true) {
             this.SPAEmulationBrowserNavBtns();
-            let root = this.base;
+            var root = this.base;
             this.base = this.base + '/#';
             if (document.location.href == root + '/') {
                 document.location = this.base;
@@ -112,104 +108,134 @@ class RouterModule {
             console.error("Error 404\nPage not found.");
         };
         this.routes = [];
-        this.DOMModule = new __WEBPACK_IMPORTED_MODULE_0__source_constrjs_dom_module_dom_module__["a" /* DOMModule */]();
+        this.DOMModule = new _constrjsDom.DOMModule();
         this.name = settings.name || 'RouterModule';
     }
-    SPAEmulationBrowserNavBtns() {
-        window.innerDocClick = false;
-        document.onmouseover = function () {
-            window.innerDocClick = true;
-        };
-        document.onmouseleave = function () {
-            window.innerDocClick = false;
-        };
-        window.onhashchange = function () {
-            if (!window.innerDocClick) {
-                location.reload();
-            }
-        };
-    }
-    add(routesSets) {
-        let that = this;
-        routesSets.forEach(set => {
-            let path = Object.keys(set)[0];
-            let callback = set[path];
-            that.routes.push({
-                _path: path,
-                _callback: callback
-            });
-        });
-        this.init();
-    }
-    navigate(_path, e) {
-        let that = this,
-            pageNotFound = 0,
-            x = 0;
-        this.routes.forEach(route => {
-            let routeParts = route._path.split('/').slice(1),
-                pathParts = _path.split('/').slice(1),
-                match = 0,
-                _arguments = {};
-            if (routeParts.length == pathParts.length) {
-                x++;
-                routeParts.forEach((part, i) => {
-                    if (/^:[da-z]{1,255}/g.test(part) === true && pathParts[i].length > 0) {
-                        let key = part.slice(1);
-                        _arguments[key] = pathParts[i];
-                        match++;
-                    } else if (part == pathParts[i]) {
-                        match++;
-                    }
-                });
-                if (match == routeParts.length && match !== 0 || _path == '') {
-                    history.pushState(null, null, that.base + _path);
-                    route._callback(_arguments, e);
-                    that.DOMModule.removeClass(document.querySelectorAll(this.navSelector + ' ' + this.aTagSelector), 'active');
-                    let activeLink = document.querySelector(this.navSelector + ' ' + this.aTagSelector + '[href="' + _path + '"');
 
-                    let href = _path;
-                    while (href.length > 0) {
-                        href = href.slice(0, href.lastIndexOf('/'));
-                        if (href != '') {
-                            let parent = document.querySelector(this.navSelector + ' ' + this.aTagSelector + '[href="' + href + '"');
-                            if (parent !== null) {
-                                that.DOMModule.addClass(parent, 'active');
+    _createClass(RouterModule, [{
+        key: 'SPAEmulationBrowserNavBtns',
+        value: function SPAEmulationBrowserNavBtns() {
+            window.innerDocClick = false;
+            document.onmouseover = function () {
+                window.innerDocClick = true;
+            };
+            document.onmouseleave = function () {
+                window.innerDocClick = false;
+            };
+            window.onhashchange = function () {
+                if (!window.innerDocClick) {
+                    location.reload();
+                }
+            };
+        }
+    }, {
+        key: 'add',
+        value: function add(routesSets) {
+            var that = this;
+            routesSets.forEach(function (set) {
+                var path = Object.keys(set)[0];
+                var callback = set[path];
+                that.routes.push({
+                    _path: path,
+                    _callback: callback
+                });
+            });
+            this.init();
+        }
+    }, {
+        key: 'navigate',
+        value: function navigate(_path, e) {
+            var _this = this;
+
+            var that = this,
+                pageNotFound = 0,
+                x = 0;
+            this.routes.forEach(function (route) {
+                var routeParts = route._path.split('/').slice(1),
+                    pathParts = _path.split('/').slice(1),
+                    match = 0,
+                    _arguments = {};
+                if (routeParts.length == pathParts.length) {
+                    x++;
+                    routeParts.forEach(function (part, i) {
+                        if (/^:[da-z]{1,255}/g.test(part) === true && pathParts[i].length > 0) {
+                            var key = part.slice(1);
+                            _arguments[key] = pathParts[i];
+                            match++;
+                        } else if (part == pathParts[i]) {
+                            match++;
+                        }
+                    });
+                    if (match == routeParts.length && match !== 0 || _path == '') {
+                        history.pushState(null, null, that.base + _path);
+                        route._callback(_arguments, e);
+                        that.DOMModule.removeClass(document.querySelectorAll(_this.navSelector + ' ' + _this.aTagSelector), 'active');
+                        var activeLink = document.querySelector(_this.navSelector + ' ' + _this.aTagSelector + '[href="' + _path + '"');
+
+                        var href = _path;
+                        while (href.length > 0) {
+                            href = href.slice(0, href.lastIndexOf('/'));
+                            if (href != '') {
+                                var parent = document.querySelector(_this.navSelector + ' ' + _this.aTagSelector + '[href="' + href + '"');
+                                if (parent !== null) {
+                                    that.DOMModule.addClass(parent, 'active');
+                                }
                             }
                         }
-                    }
 
-                    if (activeLink !== null) {
-                        that.DOMModule.addClass(activeLink, 'active');
+                        if (activeLink !== null) {
+                            that.DOMModule.addClass(activeLink, 'active');
+                        }
+                    } else {
+                        pageNotFound++;
                     }
-                } else {
-                    pageNotFound++;
                 }
-            }
-        });
-        if (pageNotFound == x) {
-            that.error404();
-            return false;
-        }
-    }
-    init() {
-        let path = document.location.href.replace(this.base, '');
-        this.navigate(path);
-        this.activateRouterLinks(this.navSelector);
-    }
-    activateRouterLinks(navSelector) {
-        let routerLinks = document.querySelectorAll(navSelector + ' ' + this.aTagSelector),
-            that = this;
-        routerLinks.forEach(link => {
-            link.addEventListener('click', function (e) {
-                e.preventDefault();
-                let href = this.getAttribute('href');
-                that.navigate(href, e);
             });
-        });
-    }
-}
-/* harmony export (immutable) */ __webpack_exports__["RouterModule"] = RouterModule;
+            if (pageNotFound == x) {
+                that.error404();
+                return false;
+            }
+        }
+    }, {
+        key: 'init',
+        value: function init() {
+            var path = document.location.href.replace(this.base, '');
+            this.navigate(path);
+            this.activateRouterLinks(this.navSelector);
+        }
+    }, {
+        key: 'activateRouterLinks',
+        value: function activateRouterLinks(navSelector) {
+            var routerLinks = document.querySelectorAll(navSelector + ' ' + this.aTagSelector),
+                that = this;
+            routerLinks.forEach(function (link) {
+                link.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    var href = this.getAttribute('href');
+                    that.navigate(href, e);
+                });
+            });
+        }
+    }]);
 
+    return RouterModule;
+}();
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+!function () {
+  "use strict";
+  var t = function t(_t, o) {
+    o = o || window;for (var i = 0; i < this.length; i++) {
+      _t.call(o, this[i], i, this);
+    }
+  };window.NodeList && !NodeList.prototype.forEach && (NodeList.prototype.forEach = t);
+}();
 
 /***/ }),
 /* 2 */
@@ -224,22 +250,22 @@ class RouterModule {
 "use strict";
 
 
-__webpack_require__(0);
+__webpack_require__(1);
 
-var _router = __webpack_require__(1);
+var _constrjsRouter = __webpack_require__(0);
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 __webpack_require__(2);
 
-// import { RouterModule } from '@zbigiman/constrjs.router.module';
+//import { RouterModule } from '../../../source/constrjs.router.module/router.module';
 
 var App = function App() {
     _classCallCheck(this, App);
 
     var app = this;
     //Init Router        
-    app.router = new _router.RouterModule({
+    app.router = new _constrjsRouter.RouterModule({
         base: routerModuleBase,
         error404: function error404() {
             app.router.navigate('');
@@ -278,72 +304,96 @@ var App = function App() {
 new App();
 
 /***/ }),
-/* 4 */,
-/* 5 */,
-/* 6 */,
-/* 7 */,
-/* 8 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 // DOMModule
 // ES6+ DOM elements manipulation | constrjs project
 // Author Zbigi Man Zbigniew Stępniewski 2017
-class DOMModule {
-    // addClass, removeClass
-    //Thanks to: http://jaketrent.com/post/addremove-classes-raw-javascript/
-    hasClass(el, className) {
-        if (el.classList) return el.classList.contains(className);else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+var DOMModule = exports.DOMModule = function () {
+    function DOMModule() {
+        _classCallCheck(this, DOMModule);
     }
-    addClass(elements, className) {
-        if (elements.length === undefined) {
-            elements = [elements];
+
+    _createClass(DOMModule, [{
+        key: 'hasClass',
+
+        // addClass, removeClass
+        //Thanks to: http://jaketrent.com/post/addremove-classes-raw-javascript/
+        value: function hasClass(el, className) {
+            if (el.classList) return el.classList.contains(className);else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
         }
-
-        elements.forEach(function (el) {
-            if (el.classList) el.classList.add(className);else if (!this.hasClass(el, className)) el.className += " " + className;
-        });
-    }
-    removeClass(elements, className) {
-
-        if (elements.length === undefined) {
-            elements = [elements];
-        }
-
-        elements.forEach(function (el) {
-            if (el.classList) el.classList.remove(className);else if (this.hasClass(el, className)) {
-                var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-                el.className = el.className.replace(reg, ' ');
+    }, {
+        key: 'addClass',
+        value: function addClass(elements, className) {
+            if (elements.length === undefined) {
+                elements = [elements];
             }
-        });
-    }
-    //\ addClass, removeClass
 
-    // Get parents
-    getParents(el) {
-        let parents = [];
-        while (el) {
-            parents.unshift(el);
-            el = el.parentNode;
+            elements.forEach(function (el) {
+                if (el.classList) el.classList.add(className);else if (!this.hasClass(el, className)) el.className += " " + className;
+            });
         }
-        return parents;
-    }
-    //\
+    }, {
+        key: 'removeClass',
+        value: function removeClass(elements, className) {
 
-    // Refresh
-    refresh(elements) {
-        if (elements.length === undefined) {
-            elements = [elements];
+            if (elements.length === undefined) {
+                elements = [elements];
+            }
+
+            elements.forEach(function (el) {
+                if (el.classList) el.classList.remove(className);else if (this.hasClass(el, className)) {
+                    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                    el.className = el.className.replace(reg, ' ');
+                }
+            });
         }
-        elements.forEach(function (el) {
-            el.innerHTML = el.innerHTML;
-        });
-    }
-    //\
+        //\ addClass, removeClass
 
-}
-/* harmony export (immutable) */ __webpack_exports__["a"] = DOMModule;
+        // Get parents
 
+    }, {
+        key: 'getParents',
+        value: function getParents(el) {
+            var parents = [];
+            while (el) {
+                parents.unshift(el);
+                el = el.parentNode;
+            }
+            return parents;
+        }
+        //\
+
+        // Refresh
+
+    }, {
+        key: 'refresh',
+        value: function refresh(elements) {
+            if (elements.length === undefined) {
+                elements = [elements];
+            }
+            elements.forEach(function (el) {
+                el.innerHTML = el.innerHTML;
+            });
+        }
+        //\
+
+    }]);
+
+    return DOMModule;
+}();
 
 /***/ })
 /******/ ]);
